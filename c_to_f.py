3023452eea1d781f3b6f04360449ed793d4be497
Anamikatk <116331823+Anamikatk@users.noreply.github.com>
2022-10-21 11:54:34 +0530

    Add files via upload

---
+++ c_to_f.py
@@ -0,0 +1,8 @@
+#convert celsius to fahrenheit
+c=int(input("Enter the temperature in degree celsius : " ))
+f=(9/5)*c+32
+print ("Corresponding degree in fahrenheit  : ", f)
+
+
+
+
---
+++ factorial.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a no. : "))
+p=1
+for i in range(1,a+1):
+    p=p*i
+print("factorial of ",a," : ",p)
+    
