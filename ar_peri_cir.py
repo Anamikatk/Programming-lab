ece64e2910abc1d3ab42757bf67a5fb59065a0a4
Anamikatk <116331823+Anamikatk@users.noreply.github.com>
2022-11-11 10:06:39 +0530

    Delete ar_peri_cir.py

--- ar_peri_cir.py
+++
@@ -1,7 +0,0 @@
-#find area and perimeter of a circle
-r=int(input("Enter the radius of the circle : "))
-area=3.14*r*r
-print("Area of the circle : ",area)
-peri=2*3.14*r
-print("Perimeter of the circle : ", peri)
-
